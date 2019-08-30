f2f94c67fc8be7f5d748e994410c261bd5a3ca69
molpopgen <krthornt@uci.edu>
2019-08-30 10:42:44 -0700

    nicen up the plots

--- plot_benchmarks.py
+++ plot_benchmarks.py
@@ -2,18 +2,28 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-for infile in ["benchmarks_including_copy.txt", "benchmarks_without_copy.txt"]:
+yfactors=[0.4,0.3]
+for infile,yf in zip(["benchmarks_including_copy.txt", "benchmarks_without_copy.txt"], yfactors):
+    print(infile, yf)
     b = pd.read_csv(infile, sep=' ')
+    b = b.round({'Nu':4})
     b['seconds_snp'] = b.seconds/b.nmutations
     print(b.seconds_snp.min())
     # p = sns.scatterplot(x='nsam', y='seconds_snp',
-    p = sns.scatterplot(x='nsam', y='seconds',
+    p = sns.scatterplot(x='nsam', y='seconds_snp',
                         hue='toolkit', style='Nu',
                         data=b, alpha= 0.5)
     plot = p.get_figure()
     #plt.ylim(0.0, 0.0006)
     plt.xscale('log')
+    ylim=plt.gca().get_ylim()
+    ylim=(ylim[0]*1e-1,ylim[1]*yf)
+    plt.ylim(*ylim)
+    plt.xlabel("Sample size (haploid)")
+    plt.ylabel("Run time per variant")
     #plt.legend(loc=2,bbox_to_anchor=(1,1))
-    of = infile.replace("txt","png")
-    plot.savefig(of, dpi=300)
+    of = infile.replace("txt","pdf")
+    plt.legend(loc='upper left',fontsize='x-small')
+    plt.tight_layout()
+    plot.savefig(of)
+    plt.clf()
