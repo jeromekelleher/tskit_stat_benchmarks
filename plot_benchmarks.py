d63824e325664d9f3ed093becc0ea1fbc06212ad
molpopgen <krthornt@uci.edu>
2019-08-30 10:01:39 -0700

    mod plotting script

--- plot_benchmarks.py
+++ plot_benchmarks.py
@@ -2,15 +2,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-b = pd.read_csv("benchmarks2_output.txt", sep=' ')
-b['seconds_snp'] = b.seconds/b.nmutations
-print(b.seconds_snp.min())
-# p = sns.scatterplot(x='nsam', y='seconds_snp',
-p = sns.scatterplot(x='nsam', y='seconds',
-                    hue='toolkit', style='Nu',
-                    data=b, alpha= 0.5)
-plot = p.get_figure()
-#plt.ylim(0.0, 0.0006)
-plt.xscale('log')
-#plt.legend(loc=2,bbox_to_anchor=(1,1))
-plot.savefig('benchmarks.png', dpi=300)
+
+for infile in ["benchmarks_including_copy.txt", "benchmarks_without_copy.txt"]:
+    b = pd.read_csv(infile, sep=' ')
+    b['seconds_snp'] = b.seconds/b.nmutations
+    print(b.seconds_snp.min())
+    # p = sns.scatterplot(x='nsam', y='seconds_snp',
+    p = sns.scatterplot(x='nsam', y='seconds',
+                        hue='toolkit', style='Nu',
+                        data=b, alpha= 0.5)
+    plot = p.get_figure()
+    #plt.ylim(0.0, 0.0006)
+    plt.xscale('log')
+    #plt.legend(loc=2,bbox_to_anchor=(1,1))
+    of = infile.replace("txt","png")
+    plot.savefig(of, dpi=300)
